33042c48c3bb85ed15a565eb79ef99a21cd7f179
Sebastian <s.gilparga@gmail.com>
2015-08-24 15:41:41 -0500

    Inicio de la funcionalidad de añadir elementos al menu dinamicamente (incompleto)

---
+++ VRClassroom GUI/Assets/Scripts/AddToMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddToMenu : MonoBehaviour {
+
+	public  GameObject MenuElement;
+
+	public void AddElement(){
+		GameObject instantElement = (GameObject)Instantiate (MenuElement, Vector3.zero, Quaternion.identity);
+		instantElement.transform.parent = gameObject.transform;
+	}
+}
